ad0fb400e22c54501f0db10572766d450f5ccc64
Toms Druvaskalns <93099357+tomsdruvas@users.noreply.github.com>
2023-09-01 22:24:28 +0100

    Add legal moves display

--- src/components/chessboard/Chessboard.js
+++ src/components/chessboard/Chessboard.js
@@ -2,15 +2,20 @@ import React, {useState} from "react";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import "./Chessboard.css"
 import {Tile} from "../tile/Tile";
+import useAuth from "../../hooks/useAuth";
 
 const BOARD_URL = "/board";
 const verticalAxis = [7, 6, 5, 4, 3, 2, 1, 0];
 const horizontalAxis = [0, 1, 2, 3, 4, 5, 6, 7];
-let squares = [];
 
 export const Chessboard = () => {
     const axiosPrivate = useAxiosPrivate();
     const [chessBoard, setChessBoard] = useState([]);
+    const [pieceSelected, setPieceSelected] = useState({});
+    const [availableMoves, setAvailableMoves] = useState([]);
+    const { auth } = useAuth();
+
+
 
     const handleStartGame = async (e) => {
         e.preventDefault();
@@ -24,7 +29,6 @@ export const Chessboard = () => {
                 }
             );
             setChessBoard(response?.data?.Squares);
-            console.log(JSON.stringify(squares));
         } catch (err) {
             console.log(JSON.stringify(err));
         }
@@ -34,12 +38,36 @@ export const Chessboard = () => {
         return chessBoard?.at(row)?.at(column)?.piece?.name;
     }
 
+    const selectPieceCallback = (pieceSelected) => {
+        if(chessBoard?.at(pieceSelected.row)?.at(pieceSelected.column)?.piece.type !== "emptypiece") {
+            setPieceSelected(pieceSelected)
+            findAvailableMoves(pieceSelected)
+            console.log(JSON.stringify("Setting piece selected active"))
+            console.log(JSON.stringify(pieceSelected))
+            // console.log(JSON.stringify(auth))
+        }
+    }
+
+    const findAvailableMoves = (pieceSelected) => {
+        if(pieceSelected) {
+            setAvailableMoves(chessBoard?.at(pieceSelected.row)?.at(pieceSelected.column)?.piece?.legalMoves.map(({row, column}) => {
+                console.log(JSON.stringify("Finding legal moves " + row + column))
+                return {row: row, column: column};
+            }))
+        }
+    }
+
     let board = [];
 
     for (let j = verticalAxis.length - 1; j >= 0; j--) {
         for (const column of horizontalAxis) {
             const pieceName = getPieceName(verticalAxis[j], column);
-            board.push(<Tile row={verticalAxis[j]} column={column} pieceName={pieceName}/>)
+            board.push(<Tile row={verticalAxis[j]}
+                             column={column}
+                             pieceName={pieceName}
+                             pieceSelected={pieceSelected}
+                             selectPieceCallback={selectPieceCallback}
+                             availableMoves={availableMoves}/>)
         }
     }
 
--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,13 +1,25 @@
 import "./Tile.css"
 
-export const Tile = ({pieceName, row, column, availableMove}) => {
+export const Tile = ({pieceName, row, column, selectPieceCallback, pieceSelected, availableMoves}) => {
     let tileColour;
-    if (availableMove) {
-        tileColour = "red"
+    if (availableMoves.find(element => element.row === row && element.column === column)) {
+        tileColour = "available"
     } else if ((row + column + 2) % 2 === 0) {
         tileColour = "white";
     } else {
         tileColour = "black";
     }
-    return <div className={`tile" + ${tileColour}-tile`}>{pieceName}</div>
+
+    const handleCallback = () => {
+        // if (Object.keys(pieceSelected).length !== 0) {
+        //     return selectPieceCallback({});
+        // } else {
+            return selectPieceCallback({
+                row: row,
+                column: column
+            });
+        // }
+    };
+
+    return <div onClick={handleCallback} className={`tile" + ${tileColour}-tile`}>{pieceName}</div>
 }
